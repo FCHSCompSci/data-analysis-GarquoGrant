d6dfc76ae48dc1e9d137e9124a717949d8733ad0
GarquoGrant <grant.gutterman@stu.jefferson.kyschools.us>
2018-12-17 08:34:02 -0500

    mmmm pasta langweeny

--- Data Setter.py
+++ Data Setter.py
@@ -1,18 +1,14 @@
 # Importations
 import csv
+from matplotlib import pyplot as plt
 
-import time
 
-import matplotlib.pyplot as plt
-
-with open('Video Games Sales') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+filename = 'videogame_sales.csv'
+with open (filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    highs = []
+    for column in reader:
+        high = round(float(column[5]), 2)
+        highs.append(column[5])
+    print(highs)
