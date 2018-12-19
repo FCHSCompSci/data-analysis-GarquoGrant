cb74b07fbfc52ac35e37c7b5b50a78762ba79be7
GarquoGrant <grant.gutterman@stu.jefferson.kyschools.us>
2018-12-19 08:34:13 -0500

    mmmm pasta langweeny

--- Data Setter.py
+++ Data Setter.py
@@ -13,7 +13,7 @@ with open (filename) as f:
         highs.append(column[5])
         # highs.sort()
     print(highs)
-y = [0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.4, 2.6, 2.8, 3, 3.2, ]
+y = [0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.4, 2.6, 2.8, 3, 3.2, 3.4, 3.6, 3.8, 4]
 fig = plt.figure(dpi=85, figsize= (10, 6))
 plt.plot(highs, c='red')
 plt.title("Shooting Games NA Earnings from 1980-2016")
